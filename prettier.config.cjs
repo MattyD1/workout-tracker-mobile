1d266c16dfeb0a90a493cce6b4a7eb23bdde5d2a
Matt Dunn <mattdunn0816@gmail.com>
2024-05-13 12:20:08 -0600

    feat: added linting with eslint

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,21 +1,21 @@
 /** @type {import('prettier').Config} */
 module.exports = {
-  endOfLine: "lf",
+  endOfLine: 'lf',
   semi: false,
   tabWidth: 2,
   singleQuote: true,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   importOrder: [
-    "^(react/(.*)$)|^(react$)",
-    "<THIRD_PARTY_MODULES>",
-    "",
-    "^[./]",
+    '^(react/(.*)$)|^(react$)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '^[./]',
   ],
   importOrderSeparation: false,
   importOrderSortSpecifiers: true,
   importOrderBuiltinModulesToTop: true,
-  importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
   importOrderMergeDuplicateImports: true,
   importOrderCombineTypeAndValueImports: true,
-  plugins: ["@ianvs/prettier-plugin-sort-imports"],
-};
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+}
